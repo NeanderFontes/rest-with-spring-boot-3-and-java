88b52798c714174985cab53164b2ed33ed43ba49
Neander Fontes <neanderffontes@gmail.com>
2024-05-02 11:41:37 +0100

    Adicionando logica e validação para obter soma de resultados

--- 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-3-and-java/src/main/java/br/com/neander/calculator/MathController.java
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-3-and-java/src/main/java/br/com/neander/calculator/MathController.java
@@ -16,7 +16,42 @@ public class MathController {
     public Double sum(
             @PathVariable(value = "numberOne") String numberOne,
             @PathVariable(value = "numberTwo") String numberTwo
-    ) {
-        return 1D;
+    ) throws Exception{
+        if (!isNumeric(numberOne) || isNumeric(numberTwo)) {
+            throw new Exception();
+        }
+        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+    }
+
+    /**
+     * Método para validar conversão String to Double
+     * @param strNumber - valor da variável a ser tratada
+     * @return valores finais em Double
+     */
+    private Double convertToDouble(String strNumber) {
+        if (strNumber == null) {
+            return 0D;
+        } else {
+        // Validar dados fracionados em formatos BR(20,0) & US(20.0)
+            String convertDoubleNumber = strNumber.replaceAll(",", ".");
+            if (isNumeric(convertDoubleNumber)) {
+                return Double.parseDouble(convertDoubleNumber);
+            }
+        }
+        return 0D;
+    }
+
+    /**
+     * Método para validar se o valor passado como parâmetro é numérico através de regex
+     * @param strNumber
+     * @return
+     */
+    private boolean isNumeric(String strNumber) {
+        if (strNumber == null) {
+            return false;
+        } else {
+            String convertDoubleNumber = strNumber.replaceAll(",", ".");
+            return convertDoubleNumber.matches("[-+]?[0-9]*\\.?[0-9]+");
+        }
     }
 }
