3c7e9dbb2cb42a84c418b309cd9cf5ae6da285ef
Neander Fontes <neanderffontes@gmail.com>
2024-05-03 15:55:00 +0100

    Acrescentando lógica de calculadora & Customizando Exception Reponse em arquivo JSON

--- 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-3-and-java/src/main/java/br/com/neander/calculator/MathController.java
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-3-and-java/src/main/java/br/com/neander/calculator/MathController.java
@@ -1,5 +1,6 @@
 package br.com.neander.calculator;
 
+import br.com.neander.exceptions.UnsupportedMathOperationException;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,13 +10,80 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MathController {
 
+    /**
+     * Método Sum para somar valores de @param de uma request HTTP
+     * @RequestMapping é uma Anotação indicando o mapeamento da Request do tipo de verbo HTTP
+     * @PathVariable é a Anotação utilizada para leitura e manipulação do parâmetro HTTP
+     * @param numberOne
+     * @param numberTwo
+     * @return - Resultado da Soma entre os valores de @param's
+     * @throws Exception
+     */
     @RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
     public Double sum(@PathVariable(value = "numberOne") String numberOne,
                       @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
         if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
-            throw new Exception();
+            throw new UnsupportedMathOperationException("Please set a numeric value!!!");
         }
         return convertToDouble(numberOne) + convertToDouble(numberTwo);
+    }
+
+    /**
+     * Método Subtração para subtrair valores de @param de uma request HTTP
+     * @RequestMapping é uma Anotação indicando o mapeamento da Request do tipo de verbo HTTP
+     * @PathVariable é a Anotação utilizada para leitura e manipulação do parâmetro HTTP
+     * @param numberOne
+     * @param numberTwo
+     * @return - Resultado da Subtração entre os valores de @param's
+     * @throws Exception
+     */
+    @RequestMapping(value = "/subtraction/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double subtraction(@PathVariable(value = "numberOne") String numberOne,
+                              @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationException("Please set a numeric value!!!");
+        }
+        return convertToDouble(numberOne) - convertToDouble(numberTwo);
+    }
+
+    /**
+     * Método Divisão para dividir valores de @param de uma request HTTP
+     * @RequestMapping é uma Anotação indicando o mapeamento da Request do tipo de verbo HTTP
+     * @PathVariable é a Anotação utilizada para leitura e manipulação do parâmetro HTTP
+     * @param numberOne
+     * @param numberTwo
+     * @return - Resultado da Divisão entre os valores de @param's
+     * @throws Exception
+     */
+    @RequestMapping(value = "/division/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double division(@PathVariable(value = "numberOne") String numberOne,
+                           @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationException("Please set a numeric value!!!");
+        } else {
+            if (convertToDouble(numberTwo) == 0) {
+                throw new UnsupportedMathOperationException("It's impossible to division with 0");
+            }
+            return convertToDouble(numberOne) / convertToDouble(numberTwo);
+        }
+    }
+
+    /**
+     * Método Multiplicação para multiplicar valores de @param de uma request HTTP
+     * @RequestMapping é uma Anotação indicando o mapeamento da Request do tipo de verbo HTTP
+     * @PathVariable é a Anotação utilizada para leitura e manipulação do parâmetro HTTP
+     * @param numberOne
+     * @param numberTwo
+     * @return - Resultado da Multiplicação entre os valores de @param's
+     * @throws Exception
+     */
+    @RequestMapping(value = "/multiplication/{numberOne}/{numberTwo}", method = RequestMethod.GET)
+    public Double multiplication(@PathVariable(value = "numberOne") String numberOne,
+                                 @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new UnsupportedMathOperationException("Please set a numeric value!!!");
+        }
+        return convertToDouble(numberOne) * convertToDouble(numberTwo);
     }
 
 
